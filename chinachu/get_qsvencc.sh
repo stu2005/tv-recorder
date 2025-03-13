8b2dc9ac79eee34fd974a92420bd0beb7d18d3bf
stu2005 <konstu8@gmail.com>
2025-03-14 07:27:44 +0900

    fix

--- chinachu/get_qsvencc.sh
+++ chinachu/get_qsvencc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 LATEST_RELEASE=$(gh api /repos/rigaya/QSVEnc/releases/latest)
 ASSETS=$(echo "$LATEST_RELEASE" | jq -r '.assets')
-DEB_FILES=$(echo "$ASSETS" | jq -r '.[] | select(.name | endswith(".deb")) | {name: .name, browser_download_url: .browser_download_url, ubuntu_version: (.name | split("_") | .[1] | split("-") | .[0]) }' | sort -t "-" -k 3 -r)
+DEB_FILES=$(echo "$ASSETS" | jq -r '.[] | select(.name | endswith(".deb")) | {name: .name, browser_download_url: .browser_download_url, ubuntu_version: (.name | split("_") | .[1] | split("-") | .[0]) }' | jq -r 'sort_by(.ubuntu_version) | reverse' )
 HIGHEST_DEB=$(echo "$DEB_FILES" | jq -r '.[0]')
 DEB_URL=$(echo "$HIGHEST_DEB" | jq -r '.browser_download_url')
 echo "Latest deb URL: $DEB_URL"
-curl -Lo/qsvencc.deb "$DEB_URL"
+curl -Lo /qsvencc.deb "$DEB_URL"
