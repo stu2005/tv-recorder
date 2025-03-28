728613ceed5d855ab1f14ebe064dd6a3b6dccb27
stu2005 <konstu8@gmail.com>
2025-03-28 10:40:31 +0900

    fix

--- epgstation/add_dmosource.sh
+++ epgstation/add_dmosource.sh
@@ -7,13 +7,13 @@ rm -rf /dmo-keyring.deb
 SOURCES_CONTENT=$(cat <<EOF
 Types: deb
 URIs: https://www.deb-multimedia.org/
-Suites: stable bullseye
+Suites: stable
 Components: main non-free
 Signed-By: /usr/share/keyrings/deb-multimedia-keyring.pgp
 
 Types: deb
 URIs: https://www.deb-multimedia.org/
-Suites: stable-backports bullseye-backports
+Suites: stable-backports
 Components: main
 Signed-By: /usr/share/keyrings/deb-multimedia-keyring.pgp
 EOF
--- epgstation/add_dmosource_testing.sh
+++ epgstation/add_dmosource_testing.sh
@@ -7,13 +7,13 @@ rm -rf /dmo-keyring.deb
 SOURCES_CONTENT=$(cat <<EOF
 Types: deb
 URIs: https://www.deb-multimedia.org/
-Suites: testing stable bullseye
+Suites: testing bullseye
 Components: main non-free
 Signed-By: /usr/share/keyrings/deb-multimedia-keyring.pgp
 
 Types: deb
 URIs: https://www.deb-multimedia.org/
-Suites: stable bullseye-backports
+Suites: bullseye-backports
 Components: main
 Signed-By: /usr/share/keyrings/deb-multimedia-keyring.pgp
 EOF
