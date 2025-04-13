4d53d0ec8e029340ae0440a5410979b5c765a361
stu2005 <konstu8@gmail.com>
2025-04-13 12:00:46 +0900

    fix

---
+++ set_edcb_channels.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd ./edcb
+cp -a "EDCB/Setting/BonDriver_mirakc(BonDriver_mirakc).ChSet4.txt" "EDCB/Setting/BonDriver_mirakc_T(BonDriver_mirakc).ChSet4.txt"
+cp -a "EDCB/Setting/BonDriver_mirakc(BonDriver_mirakc).ChSet4.txt" "EDCB/Setting/BonDriver_mirakc_S(BonDriver_mirakc).ChSet4.txt"
+nano "EDCB/Setting/BonDriver_mirakc_T(BonDriver_mirakc).ChSet4.txt"
+nano "EDCB/Setting/BonDriver_mirakc_S(BonDriver_mirakc).ChSet4.txt"
