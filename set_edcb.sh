216945291f0ce05c94b9c3e35551180ca0f350a7
stu2005 <konstu8@gmail.com>
2026-02-24 13:34:33 +0000

    edcbを再びサブモジュールに、edcbのDockerイメージをGitHub Actionsでビルド、compose.yamlを再び一つにまとめる

--- set_edcb.sh
+++ set_edcb.sh
@@ -7,5 +7,4 @@ cp ./EDCB/Common.example.ini ./EDCB/Common.ini
 cp ./EDCB/EpgDataCap_Bon.example.ini ./EDCB/EpgDataCap_Bon.ini
 cp ./EDCB/EpgTimerSrv.example.ini ./EDCB/EpgTimerSrv.ini
 cp ./EDCB/RecName/RecName_Macro.dll.example.ini ./EDCB/RecName/RecName_Macro.dll.ini
-nano ./wine-mount.sh
 cd ../
