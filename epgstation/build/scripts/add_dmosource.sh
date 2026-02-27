e536b994ec1f74893fc4f98c0a50e8f90b631c0e
stu2005 <konstu8@gmail.com>
2026-02-27 14:07:54 +0000

    fix

--- epgstation/build/scripts/add_dmosource.sh
+++ epgstation/build/scripts/add_dmosource.sh
@@ -7,45 +7,25 @@ rm -rf /dmo-keyring.deb
 SOURCES_CONTENT=$(cat <<EOF
 Types: deb
 URIs: https://www.deb-multimedia.org/
-Suites: trixie bookworm bullseye
+Suites: bookworm
 Components: main non-free
 Signed-By: /usr/share/keyrings/deb-multimedia-keyring.pgp
 
 Types: deb
 URIs: https://www.deb-multimedia.org/
-Suites: bookworm-backports bullseye-backports
+Suites: bookworm-backports
 Components: main
 Signed-By: /usr/share/keyrings/deb-multimedia-keyring.pgp
-
-Types: deb
-URIs: http://deb.debian.org/debian/
-Suites: bookworm bookworm-updates bookworm-backports
-Components: contrib non-free non-free-firmware
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
-
-Types: deb
-URIs: http://deb.debian.org/debian/
-Suites: trixie trixie-updates trixie-backports
-Components: main contrib non-free non-free-firmware
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 EOF
 )
 
-PREFERENCE=$(cat <<EOF
-Package: *
-Pin: release a=trixie*
-Pin-Priority: 100
-
-Package: libc6 libc-bin libstdc++6 base-files
-Pin: release a=trixie
-Pin-Priority: 500
-
-Package: *
-Pin: release o=Unofficial Multimedia Packages, a=*backports
-Pin-Priority: 500
-EOF
-)
+#PREFERENCE=$(cat <<EOF
+#Package: *
+#Pin: release o=Unofficial Multimedia Packages, a=*backports
+#Pin-Priority: 500
+#EOF
+#)
 
 echo "$SOURCES_CONTENT" >/etc/apt/sources.list.d/dmo.sources
-echo "$PREFERENCE" >/etc/apt/preferences.d/dmo.pref
+#echo "$PREFERENCE" >/etc/apt/preferences.d/dmo.pref
 apt-get update -q
